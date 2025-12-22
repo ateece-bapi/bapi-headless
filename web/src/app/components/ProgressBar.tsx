62bb3f0ac7cd06f6cc688a1f4fdc774f211efacb
ateece-bapi <ateece@bapisensors.com>
2025-12-22 14:40:06 -0600

    UX: Add fade-out animation to MegaMenu and NProgress for smooth navigation

---
+++ web/src/app/components/ProgressBar.tsx
@@ -0,0 +1,22 @@
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
+NProgress.configure({ showSpinner: false, speed: 400, minimum: 0.15, trickleSpeed: 200 });
+
+export default function ProgressBar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    NProgress.start();
+    return () => {
+      NProgress.done();
+    };
+    // Only run on pathname change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
+
+  return null;
+}

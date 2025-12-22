a7043e4970b4e08dd11b157d62ba8549a4733156
ateece-bapi <ateece@bapisensors.com>
2025-12-22 14:40:06 -0600

    UX: Add fade-out animation to MegaMenu and NProgress for smooth navigation

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProgressBar from "./components/ProgressBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -62,6 +63,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ProgressBar />
         <Header />
         {children}
         <Footer />

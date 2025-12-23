f8284f44d50e4e944573f54599483ca92974e766
ateece-bapi <ateece@bapisensors.com>
2025-12-23 12:33:43 -0600

    refactor: reorganize component structure - move layout, ui, and feature components to dedicated directories

--- web/src/app/components/index.ts
+++ web/src/app/components/index.ts
@@ -1,6 +1,6 @@
-export { default as BackToTop } from './BackToTop';
-export { default as BapiButton } from './BapiButton';
-export { default as Footer } from './Footer';
-export { default as TaglineRotator } from './TaglineRotator';
-export * from './Header';
-export * from './Hero';
+export { default as BackToTop } from '@/components/layout/BackToTop';
+export { default as BapiButton } from '@/components/ui/BapiButton';
+export { default as Footer } from '@/components/layout/Footer';
+export { default as TaglineRotator } from '@/components/ui/TaglineRotator';
+export * from '@/components/layout/Header';
+export * from '@/components/Hero';

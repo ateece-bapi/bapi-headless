416a28ce4ab85f17e15068e8c398bcaf3c2843b8
ateece-bapi <ateece@bapisensors.com>
2026-04-16 15:37:35 -0500

    style: improve WAM logo spacing in mega-menu
    - Increased padding from p-4 to p-8 for more breathing room
    - Added max-width constraint (200px) to prevent logo from appearing too large
    - Logo now has better visual balance within the featured box

--- web/src/components/layout/Header/components/MegaMenuItem.tsx
+++ web/src/components/layout/Header/components/MegaMenuItem.tsx
@@ -304,14 +304,16 @@ const MegaMenuItemComponent: React.FC<MegaMenuItemProps> = ({
                     )}
                   >
                     {item.megaMenu.featured.logo ? (
-                      <div className="absolute inset-0 flex items-center justify-center p-4">
-                        <Image
-                          src={item.megaMenu.featured.logo}
-                          alt={item.megaMenu.featured.title}
-                          fill
-                          className="object-contain"
-                          sizes="(max-width: 768px) 100vw, 300px"
-                        />
+                      <div className="absolute inset-0 flex items-center justify-center p-8">
+                        <div className="relative h-full w-full max-w-[200px]">
+                          <Image
+                            src={item.megaMenu.featured.logo}
+                            alt={item.megaMenu.featured.title}
+                            fill
+                            className="object-contain"
+                            sizes="(max-width: 768px) 200px, 200px"
+                          />
+                        </div>
                       </div>
                     ) : (
                       <div className="absolute inset-0 flex items-center justify-center">
